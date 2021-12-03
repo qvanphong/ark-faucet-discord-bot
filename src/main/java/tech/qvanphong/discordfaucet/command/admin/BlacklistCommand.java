f2ac03d38e3e27c9defed9e599f56d3a5488bee0
qvanphong <qvanphong@gmail.com>
2021-12-03 10:44:21 +0700

    add check is admin before using /blacklist

--- src/main/java/tech/qvanphong/discordfaucet/command/admin/BlacklistCommand.java
+++ src/main/java/tech/qvanphong/discordfaucet/command/admin/BlacklistCommand.java
@@ -28,40 +28,44 @@ public class BlacklistCommand implements SlashCommand {
     public Mono<Void> handle(ChatInputInteractionEvent event) {
 
         return event.deferReply()
-                .then(Mono.just(event.getOptions().get(0)))
-                .flatMap(subCommandInteraction -> {
+                .then(Mono.just(userUtility.isAdmin(event.getInteraction().getUser().getId().asLong())))
+                .flatMap(isAdmin -> !isAdmin ?
+                        event.editReply("Bạn không có quyền sử dụng lệnh này") :
+                        Mono.just(event.getOptions().get(0))
+                                .flatMap(subCommandInteraction -> {
 
-                    Optional<Snowflake> guildIdOptional = event.getInteraction().getGuildId();
-                    if (guildIdOptional.isEmpty()) return event.editReply("Không thể lấy được guild id");
+                                    Optional<Snowflake> guildIdOptional = event.getInteraction().getGuildId();
+                                    if (guildIdOptional.isEmpty())
+                                        return event.editReply("Không thể lấy được guild id");
 
-                    String subCommandName = subCommandInteraction.getName();
+                                    String subCommandName = subCommandInteraction.getName();
 
-                    long guildId = guildIdOptional.get().asLong();
+                                    long guildId = guildIdOptional.get().asLong();
 
-                    return subCommandInteraction
-                            .getOptions()
-                            .get(0)
-                            .getValue()
-                            .get()
-                            .asUser()
-                            .flatMap(targetUser -> {
-                                long userId = targetUser.getId().asLong();
-                                switch (subCommandName) {
-                                    case "add":
-                                        if (userUtility.isUserBlackListed(userId, guildId))
-                                            return event.editReply(targetUser.getUserData().username() + " đã có trong danh sách cấm");
-                                        userUtility.createBlacklistUser(userId, guildId);
-                                        return event.editReply("Đã thêm " + targetUser.getUserData().username() + " vào danh sách cấm.");
+                                    return subCommandInteraction
+                                            .getOptions()
+                                            .get(0)
+                                            .getValue()
+                                            .get()
+                                            .asUser()
+                                            .flatMap(targetUser -> {
+                                                long userId = targetUser.getId().asLong();
+                                                switch (subCommandName) {
+                                                    case "add":
+                                                        if (userUtility.isUserBlackListed(userId, guildId))
+                                                            return event.editReply(targetUser.getUserData().username() + " đã có trong danh sách cấm");
+                                                        userUtility.createBlacklistUser(userId, guildId);
+                                                        return event.editReply("Đã thêm " + targetUser.getUserData().username() + " vào danh sách cấm.");
 
-                                    case "remove":
-                                        if (userUtility.removeBlacklistUser(userId, guildId)) {
-                                            return event.editReply("Đã xóa " + targetUser.getUserData().username() + " ra khỏi danh sách");
-                                        }
-                                        return event.editReply("Xóa khỏi danh sách cấm thất bại, người dùng không có trong danh sách cấm");
-                                }
-                                return Mono.empty();
-                            });
-                })
+                                                    case "remove":
+                                                        if (userUtility.removeBlacklistUser(userId, guildId)) {
+                                                            return event.editReply("Đã xóa " + targetUser.getUserData().username() + " ra khỏi danh sách");
+                                                        }
+                                                        return event.editReply("Xóa khỏi danh sách cấm thất bại, người dùng không có trong danh sách cấm");
+                                                }
+                                                return Mono.empty();
+                                            });
+                                }))
                 .then();
     }
 }
