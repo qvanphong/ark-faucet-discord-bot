162f0f66aec443f4865cb76e224aa0327e0f357c
qvanphong <qvanphong@gmail.com>
2021-12-10 17:12:47 +0700

    Add guild owner as admin of bot when added to server. Remove admins from guild if bot leave.

--- src/main/java/tech/qvanphong/discordfaucet/listener/JoinGuildListener.java
+++ src/main/java/tech/qvanphong/discordfaucet/listener/JoinGuildListener.java
@@ -4,27 +4,40 @@ import discord4j.core.event.domain.guild.GuildCreateEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import tech.qvanphong.discordfaucet.entity.Admin;
+import tech.qvanphong.discordfaucet.entity.User;
 import tech.qvanphong.discordfaucet.service.GuildConfigService;
+import tech.qvanphong.discordfaucet.utility.UserUtility;
 
 @Component
 public class JoinGuildListener {
     private GuildConfigService guildConfigService;
+    private UserUtility userUtility;
 
     @Autowired
-    public JoinGuildListener(GuildConfigService guildConfigService) {
+    public JoinGuildListener(GuildConfigService guildConfigService, UserUtility userUtility) {
         this.guildConfigService = guildConfigService;
+        this.userUtility = userUtility;
     }
 
     public Mono<Void> handle(GuildCreateEvent event) {
         return Mono.just(event)
                 .flatMap(guildCreateEvent -> {
                     System.out.println("Joined guild " + guildCreateEvent.getGuild().getName());
+
                     long guildId = guildCreateEvent.getGuild().getId().asLong();
                     if (guildConfigService.getGuildConfig(guildId) == null) {
                         guildConfigService.createNewGuildConfig(guildId);
                     }
+                    long ownerId = guildCreateEvent.getGuild().getOwnerId().asLong();
+                    if (!userUtility.isAdmin(ownerId, guildId)) {
+                        User user = userUtility.getOrCreateUser(ownerId);
+                        Admin admin = new Admin();
+                        admin.setUser(user);
+                        admin.setGuildId(guildId);
 
-//                    this.tokenConfigReader.readTokenConfig(guildId);
+                        userUtility.createAdmin(admin);
+                    }
 
                     return Mono.empty();
                 });
--- src/main/java/tech/qvanphong/discordfaucet/listener/QuitGuildListener.java
+++ src/main/java/tech/qvanphong/discordfaucet/listener/QuitGuildListener.java
@@ -5,14 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import tech.qvanphong.discordfaucet.service.GuildConfigService;
+import tech.qvanphong.discordfaucet.utility.UserUtility;
 
 @Component
 public class QuitGuildListener {
     private GuildConfigService guildConfigService;
+    private UserUtility userUtility;
 
     @Autowired
-    public QuitGuildListener(GuildConfigService guildConfigService) {
+    public QuitGuildListener(GuildConfigService guildConfigService,UserUtility userUtility ) {
         this.guildConfigService = guildConfigService;
+        this.userUtility = userUtility;
     }
 
     public Mono<Void> handle(GuildDeleteEvent event) {
@@ -22,6 +25,7 @@ public class QuitGuildListener {
                         long guildId = guildDeleteEvent.getGuild().get().getId().asLong();
                         System.out.println("Quit guild " + guildDeleteEvent.getGuild().get().getName());
                         guildConfigService.removeGuildConfig(guildId);
+                        userUtility.removeAdminFromGuild(guildId);
                     }
 
                     return Mono.empty();
